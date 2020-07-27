2db22dcfeb983a9ebb599dc89901af883f36720d
Usiel Alor <iscuas@gmail.com>
2020-07-26 23:00:41 -0500

    version 0.0.0.1

---
+++ src/app/@widgets/image-item/image-item.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageItemComponent } from './image-item.component';
+
+
+
+@NgModule({
+  declarations: [ImageItemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImageItemModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarModule } from './@components/navbar/navbar.module';
+import { HeaderModule } from './@components/header/header.module';
+import { FooterModule } from './@components/footer/footer.module';
+import { PortfolioModule } from './@components/portfolio/portfolio.module';
+import { CopyrightModule } from './@components/copyright/copyright.module';
+import { ContactModule } from './@components/contact/contact.module';
+import { AboutModule } from './@components/about/about.module';
+import { SearchModule } from './@components/search/search.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarModule,
+    HeaderModule,
+    SearchModule,
+    FooterModule,
+    PortfolioModule,
+    CopyrightModule,
+    ContactModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
